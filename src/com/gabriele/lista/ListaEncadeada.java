daf0e0ca55f9cd2d5372ac7d0802837af278f76d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-03-15 15:43:54 -0300

    feat: cria propriedade para tamanho da ListaEncadeada

--- src/com/gabriele/lista/ListaEncadeada.java
+++ src/com/gabriele/lista/ListaEncadeada.java
@@ -3,6 +3,7 @@ package com.gabriele.lista;
 public class ListaEncadeada<T> {
     //0 - Crio uma celula de início. Como se fosse a porta do trem.
     private No<T> inicio;
+    private int tamanho; //4 - propriedade para ter a quantidade de elementos na lista encadeada.
 
     //1- Recebo um elemento de um tipo qualquer
     public void adiciona(T elemento) {
@@ -12,6 +13,11 @@ public class ListaEncadeada<T> {
 
         //3 - Preciso apontar o início (porta do trem) para o proximo elemento (vagão do trem)
         this.inicio = celula; //quer dizer que esse inicio guarda o ponteiro da celula.
+        this.tamanho++;
+    }
+
+    public int getTamanho() {
+        return this.tamanho;
     }
 
     @Override
