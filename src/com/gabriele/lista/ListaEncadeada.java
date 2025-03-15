f43a1022d29c8880b3b1e92d6e9abaa2c9d3481d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-03-15 16:11:26 -0300

    feat: adiciona No para ultimo elemento da listaEncadeada

--- src/com/gabriele/lista/ListaEncadeada.java
+++ src/com/gabriele/lista/ListaEncadeada.java
@@ -1,18 +1,20 @@
 package com.gabriele.lista;
 
 public class ListaEncadeada<T> {
-    //0 - Crio uma celula de início. Como se fosse a porta do trem.
+    //0 - Crio um No de início que é uma referência para o inicio da ListaEncadeada.
     private No<T> inicio;
+    private No<T> ultimo;
     private int tamanho; //4 - propriedade para ter a quantidade de elementos na lista encadeada.
 
     //1- Recebo um elemento de um tipo qualquer
     public void adiciona(T elemento) {
-        //2 - Instancio esse elemento como um No chamado celula (um vagão do trem), e ele aponta para null conforme o
-        // construtor está configurado.
+        //2 - Esse elemento instacia um No que já aponta para null, que é o proximo espaço da lista.
         No<T> celula = new No<T>(elemento);
 
-        //3 - Preciso apontar o início (porta do trem) para o proximo elemento (vagão do trem)
-        this.inicio = celula; //quer dizer que esse inicio guarda o ponteiro da celula.
+        //3 - Defino então que o inicio da lista é essa celula que criei acima.
+        this.inicio = celula; //Inicio (que é uma instacia de No) está guardando o ponteiro do começo da lista.
+        this.ultimo = celula; //se nossa Lista possui apenas um elemento ele também será o último, ou seja Ultimo
+        // guarda o ponteiro para o ultimo elemento da lista.
         this.tamanho++;
     }
 
