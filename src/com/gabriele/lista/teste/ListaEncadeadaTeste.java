ad2dd1d88202c6b1abfee415add22e317c46c9c8
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-03-15 15:23:55 -0300

    feat: cria a classe ListaEncadeadaTeste

--- src/com/gabriele/lista/teste/ListaEncadeadaTeste.java
+++ src/com/gabriele/lista/teste/ListaEncadeadaTeste.java
@@ -1,2 +1,13 @@
-package com.gabriele.lista.teste;public class ListaEncadeadaTeste {
+package com.gabriele.lista.teste;
+
+import com.gabriele.lista.ListaEncadeada;
+
+public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.adiciona(1);
+
+        System.out.println(lista);
+    }
 }
