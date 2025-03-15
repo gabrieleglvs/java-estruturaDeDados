56a39bea656da2fc2cc6db5894f80e5264d1b752
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-03-15 17:21:04 -0300

    feat: verifica listEncadeada e insere proximo elemento

--- src/com/gabriele/lista/ListaEncadeada.java
+++ src/com/gabriele/lista/ListaEncadeada.java
@@ -11,8 +11,14 @@ public class ListaEncadeada<T> {
         //2 - Esse elemento instacia um No que já aponta para null, que é o proximo espaço da lista.
         No<T> celula = new No<T>(elemento);
 
-        //3 - Defino então que o inicio da lista é essa celula que criei acima.
-        this.inicio = celula; //Inicio (que é uma instacia de No) está guardando o ponteiro do começo da lista.
+        //5 - Verificar se há elementos na lista para entender onde posicionar o próximo.
+        if(this.tamanho == 0) {
+            //3 - Defino então que o inicio da lista é essa celula que criei acima.
+            this.inicio = celula; //Inicio (que é uma instacia de No) está guardando o ponteiro do começo da lista.
+        } else {
+            this.ultimo.setProximo(celula);
+        }
+
         this.ultimo = celula; //se nossa Lista possui apenas um elemento ele também será o último, ou seja Ultimo
         // guarda o ponteiro para o ultimo elemento da lista.
         this.tamanho++;
